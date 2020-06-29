06a6c44e327d772dee916cac99266f7c1b354bfc
Marco Adkins <marcointerviews21@gmail.com>
2020-06-29 09:07:23 -0400

    [CH-2] Respond with estimated population for unknown years
    Define helper methods Population#estimated_population, Population#closest_past_population_for, Population#closest_future_population_for, Population#population_increase_per_year
    Use Population#closest_past_population_for for years greater than max

--- app/models/population.rb
+++ app/models/population.rb
@@ -14,12 +14,30 @@ class Population < ApplicationRecord
     if year < min_year
       0
     elsif year > max_year
-      Population.find_by(year: Date.new(max_year))&.population
+      closest_past_population_for(year).population
     else
-      Population.where('year <= ?', Date.new(year))
-                .order(year: :desc)
-                .limit(1)
-                .first&.population
+      Population.find_by_year(Date.new(year))&.population || estimated_population(year)
     end
+  end
+
+  def self.estimated_population(year)
+    past_pop = closest_past_population_for(year)
+    future_pop = closest_future_population_for(year)
+    population_increase_per_year = population_increase_per_year(past_pop, future_pop)
+    years_after_past_pop = year - past_pop.year.year
+
+    past_pop.population + population_increase_per_year * years_after_past_pop
+  end
+
+  def self.closest_past_population_for(year)
+    Population.where("year <= ?", Date.new(year)).order(year: :desc).limit(1).first
+  end
+
+  def self.closest_future_population_for(year)
+    Population.where("year >= ?", Date.new(year)).order(:year).limit(1).first
+  end
+
+  def self.population_increase_per_year(past_pop, future_pop)
+    (future_pop.population - past_pop.population) / (future_pop.year.year - past_pop.year.year)
   end
 end
