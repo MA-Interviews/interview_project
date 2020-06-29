8db2280d15a844f0333f807d67f4e72067315d0f
Marco Adkins <marcointerviews21@gmail.com>
2020-06-29 09:00:03 -0400

    [CH-2] Update population model rspecs
    Add test for estimated population for unknown years
    Use variables to make tests more manageable

--- spec/models/population_spec.rb
+++ spec/models/population_spec.rb
@@ -1,26 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Population, type: :model do
+  let(:first_known_year) { Population.min_year }
+  let(:first_known_population) { Population.find_by_year(Date.new(first_known_year)).population }
+  let(:first_known_population_increase_per_year) { 1601632 }
+
+  let(:last_known_year) { Population.max_year }
+  let(:last_known_population) { Population.find_by_year(Date.new(last_known_year)).population }
 
   it "should accept a year we know and return the correct population" do
-    expect(Population.get(1900)).to eq(76212168)
-    expect(Population.get(1990)).to eq(248709873)
+    expect(Population.get(first_known_year)).to eq(first_known_population)
+    expect(Population.get(last_known_year)).to eq(last_known_population)
   end
 
-  it "should accept a year we don't know and return the previous known population" do
-    expect(Population.get(1902)).to eq(76212168)
-    expect(Population.get(1908)).to eq(76212168)
+  it "should accept a year we don't know and return an estimated population" do
+    expect(Population.get(first_known_year + 2)).to eq(first_known_population + first_known_population_increase_per_year * 2)
+    expect(Population.get(first_known_year + 8)).to eq(first_known_population + first_known_population_increase_per_year * 8)
   end
 
   it "should accept a year that is before earliest known and return zero" do
-    expect(Population.get(1800)).to eq(0)
+    expect(Population.get(first_known_year - 1)).to eq(0)
     expect(Population.get(0)).to eq(0)
     expect(Population.get(-1000)).to eq(0)
   end
 
   it "should accept a year that is after latest known and return the last known population" do
-    expect(Population.get(2000)).to eq(248709873)
-    expect(Population.get(200000)).to eq(248709873)
+    expect(Population.get(last_known_year + 1)).to eq(last_known_population)
+    expect(Population.get(last_known_year + 1000)).to eq(last_known_population)
   end
 
 end
