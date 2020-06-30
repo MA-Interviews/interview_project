e97eeb6b2d3a7a78247c8fbb45cee04f1fe5fc71
Marco Adkins <marcointerviews@gmail.com>
2020-06-29 21:29:35 -0400

    [CH-5] Update future year unit tests to expect estimated population

--- spec/models/population_spec.rb
+++ spec/models/population_spec.rb
@@ -8,6 +8,12 @@ RSpec.describe Population, type: :model do
   let(:last_known_year) { Population.max_year }
   let(:last_known_population) { Population.find_by_year(Date.new(last_known_year)).population }
 
+  let(:future_year) { last_known_year + 1 }
+  let(:future_year_population) { 271_093_761 }
+
+  let(:max_future_year) { Population::MAX_FUTURE_YEAR }
+  let(:max_future_year_population) { 3_042_334_591_789_847_255_877_419_008 }
+
   it "should accept a year we know and return the correct population" do
     expect(Population.get(first_known_year)).to eq(first_known_population)
     expect(Population.get(last_known_year)).to eq(last_known_population)
@@ -24,9 +30,9 @@ RSpec.describe Population, type: :model do
     expect(Population.get(-1000)).to eq(0)
   end
 
-  it "should accept a year that is after latest known and return the last known population" do
-    expect(Population.get(last_known_year + 1)).to eq(last_known_population)
-    expect(Population.get(last_known_year + 1000)).to eq(last_known_population)
+  it "should accept a year that is after latest known and return an estimated population" do
+    expect(Population.get(future_year)).to eq(future_year_population)
+    expect(Population.get(max_future_year)).to eq(max_future_year_population)
   end
 
 end
