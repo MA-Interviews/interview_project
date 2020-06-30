879b3ef69f46142f2f2ce22f609cfa6b0c5de32d
Marco Adkins <marcointerviews@gmail.com>
2020-06-29 21:51:34 -0400

    [CH-5] Prevent year inputs greater than 2500 and render error message

--- app/controllers/populations_controller.rb
+++ app/controllers/populations_controller.rb
@@ -1,4 +1,8 @@
 class PopulationsController < ApplicationController
+  before_action do
+    redirect_to populations_path, alert: "Year must be less than: #{Population::MAX_FUTURE_YEAR}" if params[:year].to_i > Population::MAX_FUTURE_YEAR
+  end
+
   def index
   end
 
